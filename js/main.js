cb33144ca699c7667c928037986c2baa044f4ac0
IAN BRUCE <ibruce@auxly.com>
2019-01-16 12:58:11 -0500

    add: newsletter signup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+"use strict";
+
 $(document).ready(function() {
-  
+
+  //* Overlay Toggle
+  //  ==========================================================================
+  var $newsletterNav,
+      $overlay,
+      $overlayClose;
+
+   $newsletterNav = $("#newsletterNavButton");
+   $overlay = $("#overlay");
+   $overlayClose = $(".overlay-close");
+
+  $newsletterNav.click(function() {
+      $overlay.addClass("active");
+      $overlay.removeClass("closed");
+  });
+
+  $overlayClose.click(function() {
+      $overlay.removeClass("active");
+      $overlay.addClass("closed");
+  });
+
+  $(document).on('click', function (e) {
+      if (
+        $(e.target).closest("#newsletter").length === 0 &&
+        $(e.target).closest("#newsletterNavButton").length === 0
+      ) {
+          $overlay.removeClass("active");
+          $overlay.addClass("closed");
+      }
+  });
+
 });
