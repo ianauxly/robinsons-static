f151ee07355782550a803254573c2149683f3da0
IAN BRUCE <ibruce@auxly.com>
2019-01-17 14:39:25 -0500

    removed: scroll disappear animation

--- js/main.js
+++ js/main.js
@@ -40,21 +40,21 @@ $(document).ready(function() {
   //* Header Disappear/Reappear on Scroll
   //  ==========================================================================
 
-  var lastScrollTop,
-      $header;
-
-  lastScrollTop = 0;
-  $header = $('header');
-
-  $(window).scroll(function(){
-     var st = $(this).scrollTop();
-     if (st > lastScrollTop){
-         // downscroll code
-         $header.addClass('hideOnScroll');
-     } else {
-        // upscroll code
-        $header.removeClass('hideOnScroll');
-     }
-     lastScrollTop = st;
-  });
+  // var lastScrollTop,
+  //     $header;
+  //
+  // lastScrollTop = 0;
+  // $header = $('header');
+  //
+  // $(window).scroll(function(){
+  //    var st = $(this).scrollTop();
+  //    if (st > lastScrollTop){
+  //        // downscroll code
+  //        $header.addClass('hideOnScroll');
+  //    } else {
+  //       // upscroll code
+  //       $header.removeClass('hideOnScroll');
+  //    }
+  //    lastScrollTop = st;
+  // });
 });
