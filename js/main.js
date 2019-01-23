334cbc87d3594ee792b9ab6cfa8675a8a0321e8e
IAN BRUCE <ibruce@auxly.com>
2019-01-23 14:31:56 -0500

    add: mobile nav

--- js/main.js
+++ js/main.js
@@ -44,9 +44,38 @@ $(document).ready(function() {
       ) {
           $overlay.removeClass("active");
           $overlay.addClass("closed");
-          $body.removeClass("scroll-pause");
+          // $body.removeClass("scroll-pause");
+      } else if (
+        $(e.target).closest("#newsletter").length === 0 &&
+        $(e.target).closest("#newsletterNavButton").length === 0 &&
+        $(e.target).closest("#menuMobileToggle").length === 0
+      ) {
+        $body.removeClass("scroll-pause");
       }
   });
+
+  //* MOBILE Overlay Toggle
+  //  ==========================================================================
+  var $mobileMenuToggleButton,
+      $mobileMenu,
+      $mobileMenuClose;
+
+      $mobileMenuToggleButton = $('#menuMobileToggle');
+      $mobileMenu = $('#mobileMenu');
+      $mobileMenuClose = $('#mobileMenu .overlay-close');
+
+      $mobileMenuToggleButton.click(function() {
+        $mobileMenu.toggleClass("active");
+        $mobileMenu.toggleClass("closed");
+        $body.toggleClass("scroll-pause");
+      });
+
+      $mobileMenuClose.click(function() {
+          $mobileMenu.removeClass("active");
+          $mobileMenu.addClass("closed");
+          $body.removeClass("scroll-pause");
+      });
+
 
   //* Header Disappear/Reappear on Scroll
   //  ==========================================================================
